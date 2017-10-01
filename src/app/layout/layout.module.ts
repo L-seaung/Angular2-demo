3a719084b6dc7e58b6767264dc54033dd8c87fe9
L-seaung <2393943608@qq.com>
2017-10-01 23:26:23 +0800

    oct 1, 23:26:20

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from './component/header/header.component';
+
+@NgModule({
+  imports: [],
+  declarations: [
+    HeaderComponent
+  ],
+  providers: [],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class LayoutModule { }
